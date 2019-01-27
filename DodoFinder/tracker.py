ba54cb774ed0e74894b5888f96d379e9608cde26
Tyler Nicewarner <tylernicewarner@yahoo.com>
2019-01-27 02:26:10 -0600

    refactored code to suit bot better

--- DodoFinder/tracker.py
+++ DodoFinder/tracker.py
@@ -1,1 +1,7 @@
+import urllib.request, json
 
+url = urllib.request.urlopen("https://atlas.reznok.com/na_pvp_players.json")
+data = json.loads(url.read().decode())
+
+with open('players.json', 'w') as outfile:
+    json.dump(data, outfile)
